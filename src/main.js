958ffc2775b6a15dbda8978dbc9baad7f59cb2ce
Eric <eric.swotinsky@gmail.com>
2018-03-22 10:31:32 -0700

    initialize project; add ajax call to main.js.

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(function(){
+  $('#weatherLocation').click(function() {
+      let city = $('#location').val();
+      $('#location').val("");
+      $.ajax({
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=992da43857993bf3ceaa761a6a8d3fcc`,
+        type: 'GET',
+        data: {
+          format: 'json'
+        },
+        success: function(response) {
+          $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+          $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+        },
+        error: function() {
+          $('#errors').text("There was an error processing your request. Please try again.")
+        }
+      });
+    });
+});
