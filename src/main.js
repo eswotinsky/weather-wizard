c146d2491f1f6340ed452a6339dd0a507697543f
Eric Swotinsky <eric.swotinsky@gmail.com>
2019-03-10 17:47:04 -0700

    UI revamp

--- src/main.js
+++ src/main.js
@@ -4,26 +4,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(function(){
-  $('#weatherLocation').click(function() {
-      let city = $('#location').val();
-      $('#location').val("");
-      $.ajax({
-        url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`,
-        type: 'GET',
-        data: {
-          format: 'json'
-        },
-        success: function(response) {
-          $('#errors').hide();
-          $('#results').show();
-          $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%.`);
-          $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-        },
-        error: function() {
-          $('#results').hide();
-          $('#errors').show();
-          $('#errors').text("There was an error processing your request. Please try again.")
-        }
-      });
+  $('#submitButton').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('#error').hide();
+        $('#results').show();
+        $('#city-name').text(`Weather forecast for ${response.name}`);
+        $('#humidity').text(`${response.main.humidity}% humidity`);
+        $('#tempCelsius').text(`${(response.main.temp - 273.15).toFixed(0)}°C`);
+        $('#tempFahrenheit').text(`${(((response.main.temp - 273.15) * 1.8) + 32).toFixed(0)}°F`);
+      },
+      error: function() {
+        $('#results').hide();
+        $('#error').show();
+        $('#error').text("There was an error processing your request. Please try again.")
+      }
     });
+  });
 });
